3f2324a3065c7c030a5747dba00186f151ecbcf4
AmjadHD <amjadhedhili@outlook.com>
2021-09-09 10:26:00 +0100

    Update

--- my_build_target.py
+++ my_build_target.py
@@ -6,4 +6,4 @@ import subprocess
 class RunInCmdCommand(sublime_plugin.WindowCommand):
 	def run(self, shell_cmd: str, **kwargs):
 		cmd = sublime.expand_variables(shell_cmd, self.window.extract_variables())
-		subprocess.call('start cmd /c \"{} & pause\"'.format(cmd), shell=True)
+		subprocess.call(f'start cmd /c \"{cmd} & pause\"', shell=True)
